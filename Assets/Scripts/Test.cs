d3f77cfef689eadc014e4f7621e6e46ab2472cf2
BitSmith5 <121252319+BitSmith5@users.noreply.github.com>
2025-09-14 15:24:40 -0700

    Folder cleanup
    -Reducing package size

--- Assets/Scripts/Test.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using Game.Enums;
-
-public class Test : MonoBehaviour
-{
-    public Weapons currentWeapon = Weapons.Dog;
-    public Player currentPlayer = Player.Ally;
-    public Pets myPets = Pets.Dog | Pets.Cat;
-}
