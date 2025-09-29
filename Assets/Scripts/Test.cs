f9247c229e0ee1d3e87b25a12d91002b87f9b208
BitSmith5 <121252319+BitSmith5@users.noreply.github.com>
2025-09-29 14:35:20 -0700

    EnumEditor clean up
    - Includes Sample Scene

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,22 +2,6 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    public enum MyEnum
-    {
-        ValueA,
-        ValueB,
-        ValueC,
-        Nick = 3,
-        Sarah = 4,
-        Riley = 5,
-        Margarita = 6,
-        Vickie = 7,
-        Boris = 8,
-        Gizmo = 9,
-        Sophie = 10,
-        Mike = 11
-    }
-
     [System.Flags]
     public enum Weapons
     {
@@ -32,11 +16,7 @@ public class Test : MonoBehaviour
         orange = 256,
         Grape = 512
     }
-
-    public MyEnum enumValue;
     public Weapons weapons;
     public Player.Character character;
     public Player.Abilities abilities;
-
-    public string playerName;
 }
